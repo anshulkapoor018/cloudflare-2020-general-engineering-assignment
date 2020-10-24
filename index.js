46cc7d9bcc8c91cd9387557b39abc9deb322ff11
Anshul Kapoor <anshulkapoor018@gmail.com>
2020-10-24 19:52:47 -0400

    Other formatters added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Router = require('./router')
 const staticHTMLEndpoint = "https://static-links-page.signalnerve.workers.dev"
 const type = "application/json;charset=UTF-8"
-const linkArray = [{"name": "link1", "url": "https://www.google.com"},{"name": "link2", "url": "https://www.google.com"},{"name": "link3", "url": "https://www.google.com"}]
+const linkArray = [{"name": "Google", "url": "https://www.google.com"},{"name": "Facebook", "url": "https://www.fb.com"},{"name": "Cloudflare", "url": "https://www.cloudflare.com"}]
 async function gatherResponse(response) {
   const { headers } = response
   const contentType = headers.get("content-type") || ""
@@ -19,20 +19,59 @@ async function gatherResponse(response) {
   }
 }
 
-const rewriter = new HTMLRewriter()
-
-class LinksTransformer {
-  constructor(links) {
-    this.links = links
+class removeDisplayNone {
+  constructor(attributeName) {
+    this.attributeName = attributeName
   }
-  
-  async element(element) {
-    const link = element.getAttribute("div#links")
-    if(link){
-      // e.addAttribute("src", snapshot.archived_snapshots.closest.url)
+  element(element) {
+    const attribute = element.getAttribute("style")
+    if (attribute) {
+      element.removeAttribute("style")
     }
   }
 }
+
+class LinkTransformer {
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+  element(element) {
+    const linkArrayTags = []
+    for (var x = 0; x < linkArray.length; x++) {
+      let content = "<a href=" + linkArray[x]["url"] + ">" + linkArray[x]["name"] + "</a>"
+      linkArrayTags.push(content)
+    }
+    element.setInnerContent(linkArrayTags.join('\n'), { html: true })
+  }
+}
+
+class ImageTransformer {
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+  element(element) {
+    console.log(element.tagName)
+    element.setAttribute("src", "https://simpleicons.org/icons/gravatar.svg")
+  }
+}
+
+class HeadingTransformer {
+  constructor(attributeName) {
+    this.attributeName = attributeName
+  }
+  element(element) {
+    console.log(element.tagName)
+    element.setInnerContent("Anshul Kapoor", { html: false })
+  }
+}
+
+
+const rewriter = new HTMLRewriter()
+  .on("div#profile", new removeDisplayNone())
+  .on("div#links", new LinkTransformer())
+  .on("img#avatar", new ImageTransformer())
+  .on("h1#name", new HeadingTransformer())
+  
 
 async function handleRequest(request) {
     const r = new Router()
